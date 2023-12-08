d8ed7f6c56cd6e98cab499cfac5987f457cf6cc8
fitzdora <33418972+fitzdora@users.noreply.github.com>
2023-12-08 11:53:15 +0000

    Setting up Logging

--- app/src/main/java/ie/setu/habitatappv20/MainActivity.kt
+++ app/src/main/java/ie/setu/habitatappv20/MainActivity.kt
@@ -2,10 +2,15 @@ package ie.setu.habitatappv20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+import timber.log.Timber.Forest.i
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Timber.plant(Timber.DebugTree())
+        i("HabitatApp Activity Started")
     }
+
 }
