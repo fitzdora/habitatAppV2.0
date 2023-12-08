b18a053df2c08de45c4dfeeada0fce5eae6775a7
fitzdora <33418972+fitzdora@users.noreply.github.com>
2023-12-08 13:52:12 +0000

    Setting up layout for AddSpecies Page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.compose.material3:material3")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.github.ajalt:timberkt:1.5.1")
     implementation("com.jakewharton.timber:timber:5.0.1")
