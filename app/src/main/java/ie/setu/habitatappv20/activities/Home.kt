e87ded1d89f3e939bd0560b1776bc4b9b66a1a8a
fitzdora <33418972+fitzdora@users.noreply.github.com>
2023-12-16 11:35:43 +0000

    Fragment functionality fixing

---
+++ app/src/main/java/ie/setu/habitatappv20/activities/Home.kt
@@ -0,0 +1,12 @@
+package ie.setu.habitatappv20.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ie.setu.habitatappv20.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
